71a6d9f5e4c23ce31c1482914f92f1f8a765339d
Michael Rhyndress <michaelrhyndress@gmail.com>
2019-01-25 22:04:29 +0000

    Updated settings.js

--- static/js/components/authentication/msal/settings.js
+++ static/js/components/authentication/msal/settings.js
@@ -1,6 +1,6 @@
 export const authConfig = {
 	authority: "https://login.microsoftonline.com/c3e32f53-cb7f-4809-968d-1cc4ccc785fe",
-    clientID: '', //Put App your Client ID here
+    clientID: '', //Put app client ID here
     cacheLocation: 'localStorage',
     scopes: [
     	"https://graph.microsoft.com/user_impersonation"
