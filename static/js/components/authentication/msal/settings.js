cccfc4482386713c32c7aebd6e2749e63caf8290
michael rhyndress <michaelrhyndress@gmail.com>
2019-06-17 12:41:07 -0400

    Update settings.js

--- static/js/components/authentication/msal/settings.js
+++ static/js/components/authentication/msal/settings.js
@@ -1,5 +1,5 @@
 export const authConfig = {
-	authority: "https://login.microsoftonline.com/c3e32f53-cb7f-4809-968d-1cc4ccc785fe",
+    authority: "https://login.microsoftonline.com/{{TENANT NAME HERE}}",
     clientID: '', //Put app client ID here
     cacheLocation: 'localStorage',
     scopes: [
